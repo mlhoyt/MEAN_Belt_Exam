c6839da12b168f3144bed8a1859a5c54ff903586
Mike Hoyt <derukimlh@gmail.com>
2017-06-23 07:50:56 -0700

    Added client app-routing.module; Implemented client app-routing.module

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+// NOTE: path-URL has NO leading '/'
+// NOTE: redirectTo-URL CAN (but does not have to) have leading '/'
+// NOTE: component.html: <a [routerLink]="['/{{URL}}', ...]">...</a>'
+// NOTE: component.ts: this._router.navigate( ['/{{URL}}', '{{URL}}'|{{VAR}}, ... ] );
+// { path: '{{URL}}', [ pathMatch: 'full', ] component: {{COMP}}Component },
+// { path: '{{URL}}', [ pathMatch: 'full', ] redirectTo: '/{{URL}}' },
+  { path: '', pathMatch: 'full', component: LoginComponent },
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot( routes ) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -11,12 +12,13 @@ import { LoginComponent } from './login/login.component';
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    AppRoutingModule,
   ],
   providers: [
   ],
